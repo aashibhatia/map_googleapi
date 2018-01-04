5f79f4d14c72fb25582e7ee8be4c2e9cabe90c4a
aashibhatia <30694737+aashibhatia@users.noreply.github.com>
2018-01-04 16:51:42 +0530

    main

--- __init__.py
+++ __init__.py
@@ -15,10 +15,30 @@
 # the License.
 #
 
-__version__ = "2.5.1-dev"
+import unittest
+import codecs
 
-from googlemaps.client import Client
-import googlemaps.exceptions
+try: # Python 3
+    from urllib.parse import urlparse, parse_qsl
+except ImportError: # Python 2
+    from urlparse import urlparse, parse_qsl
 
-# Allow sphinx to pick up these symbols for the documentation.
-__all__ = ["Client"]
+
+class TestCase(unittest.TestCase):
+
+    def assertURLEqual(self, first, second, msg=None):
+        """Check that two arguments are equivalent URLs. Ignores the order of
+        query arguments.
+        """
+        first_parsed = urlparse(first)
+        second_parsed = urlparse(second)
+        self.assertEqual(first_parsed[:3], second_parsed[:3], msg)
+
+        first_qsl = sorted(parse_qsl(first_parsed.query))
+        second_qsl = sorted(parse_qsl(second_parsed.query))
+        self.assertEqual(first_qsl, second_qsl, msg)
+
+    def u(self, string):
+        """Create a unicode string, compatible across all versions of Python."""
+        # NOTE(cbro): Python 3-3.2 does not have the u'' syntax.
+        return codecs.unicode_escape_decode(string)[0]
